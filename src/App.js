5b3437403439f4af43cd6b099b42850a376ca6ec
shahpranaf <shahpranaf@gmail.com>
2019-10-19 21:31:09 +0530

    Added Routes and withRouter()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import "./App.css";
+
+const HatsPage = () => {
+    return (
+        <div>
+            <h1>Hats Page</h1>
+        </div>
+    );
+};
 
 function App() {
     return (
         <div className="App">
-            <HomePage />
+            <Switch>
+                <Route exact={true} path="/" component={HomePage} />
+                <Route exact={true} path="/shop/hats" component={HatsPage} />
+            </Switch>
         </div>
     );
 }
