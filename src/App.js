a67e5567fb488331bc6bb57b9702af1b7e2f3d43
shahpranaf <shahpranaf@gmail.com>
2019-10-19 22:53:02 +0530

    Updating our shop page, added collection preview page and collection preview items

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shopPage/shoppage.component";
 import "./App.css";
-
-const HatsPage = () => {
-    return (
-        <div>
-            <h1>Hats Page</h1>
-        </div>
-    );
-};
 
 function App() {
     return (
         <div className="App">
             <Switch>
                 <Route exact={true} path="/" component={HomePage} />
-                <Route exact={true} path="/shop/hats" component={HatsPage} />
+                <Route exact={true} path="/shop" component={ShopPage} />
             </Switch>
         </div>
     );
