0a0710472cac67a5610affc244b8928ecff2c634
shahpranaf <shahpranaf@gmail.com>
2019-10-20 12:07:13 +0530

    Added header component,  menus and logo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shopPage/shoppage.component";
 import "./App.css";
+import Header from "./components/header/header.component";
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <Switch>
                 <Route exact={true} path="/" component={HomePage} />
                 <Route exact={true} path="/shop" component={ShopPage} />
