a652765bf00c6c2ae115a0fdeca48ee79d0de77f
Pranav Shah <shahpranaf@gmail.com>
2019-10-21 21:36:01 +0530

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,10 +3,10 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-    apiKey: "AIzaSyAKU_iTxZRUgxTy3gBfL3exY1KWFP4zbJU",
+    apiKey: "sdfsfwer12q3eweefsdf",
     authDomain: "crwn-db-a52c9.firebaseapp.com",
-    databaseURL: "https://crwn-db-a52c9.firebaseio.com",
-    projectId: "crwn-db-a52c9",
+    databaseURL: "32423r3rwerwer",
+    projectId: "3wer233rwer",
     storageBucket: "crwn-db-a52c9.appspot.com",
     messagingSenderId: "944602953804",
     appId: "1:944602953804:web:c2a64c4bbbcc2d11b5dce9"
