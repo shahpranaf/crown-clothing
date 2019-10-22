bb032502b9616214c4a541aa065953be9aaf9004
shahpranaf <shahpranaf@gmail.com>
2019-10-22 17:30:22 +0530

    Redux setup, added user reducer and actions, created store and connected

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const config = {
-    apiKey: "AIzaSyAKU_iTxZRUgxTy3gBfL3exY1KWFP4zbJU",
-    authDomain: "crwn-db-a52c9.firebaseapp.com",
-    databaseURL: "https://crwn-db-a52c9.firebaseio.com",
-    projectId: "crwn-db-a52c9",
-    storageBucket: "crwn-db-a52c9.appspot.com",
-    messagingSenderId: "944602953804",
-    appId: "1:944602953804:web:c2a64c4bbbcc2d11b5dce9"
-};
+const config = {};
 
 firebase.initializeApp(config);
 
