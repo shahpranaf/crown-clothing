c5105ad15a079ea398f6f57f9eab7c8a4fc2ca76
shahpranaf <shahpranaf@gmail.com>
2019-10-24 22:57:34 +0530

    added config

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const CONFIG = {
+    apiKey: "AIzaSyAKU_iTxZRUgxTy3gBfL3exY1KWFP4zbJU",
+    authDomain: "crwn-db-a52c9.firebaseapp.com",
+    databaseURL: "https://crwn-db-a52c9.firebaseio.com",
+    projectId: "crwn-db-a52c9",
+    storageBucket: "crwn-db-a52c9.appspot.com",
+    messagingSenderId: "944602953804",
+    appId: "1:944602953804:web:c2a64c4bbbcc2d11b5dce9",
+    publishKey: "pk_test_bGMZs7k6QU1YEfU2d8LvUY7V"
+};
